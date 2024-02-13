3b8f8cd423f98b0c87cf11739d876cc01a117670
Panikphil <panikphil@gmail.com>
2024-02-12 20:50:42 -0500

    materials feature for the hat plus shape option

--- src/projectCore/Hat.java
+++ src/projectCore/Hat.java
@@ -4,5 +4,7 @@ public class Hat {
 
     //new cool hat
 
-    //now you can change the color of the hat
+    //now you can change the color of the hat and shape
+
+    //new materials are available
 }
